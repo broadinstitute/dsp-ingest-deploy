f6a47bb6ae8eddf773dc75252c77cb53db3acf6b
Dan Moran <danmoran@broadinstitute.org>
2019-04-26 17:34:33 -0400

    Bump agent Java mem.

--- init-containers/biogen-aws-to-gcp-agent/transporter-aws-to-gcp-agent/entrypoint.sh
+++ init-containers/biogen-aws-to-gcp-agent/transporter-aws-to-gcp-agent/entrypoint.sh
@@ -5,8 +5,8 @@ declare -ra JAVA_OPTS_ARRAY=(
    -Dconfig.file=/etc/application.conf
    # Constrain memory.
    -server
-   -Xmx256m
-   -Xmx256m
+   -Xmx512m
+   -Xmx512m
    # GC settings (copied from Clio for now).
    -XX:+PrintFlagsFinal
    -XX:+PrintGCTimeStamps
