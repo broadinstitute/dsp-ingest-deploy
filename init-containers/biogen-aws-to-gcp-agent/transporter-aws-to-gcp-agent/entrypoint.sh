c6fa8aafad1fe37f39e9c97934ca2825eefe0995
Dan Moran <danmoran@broadinstitute.org>
2019-04-27 15:09:00 -0400

    Try even more heap for agents.

--- init-containers/biogen-aws-to-gcp-agent/transporter-aws-to-gcp-agent/entrypoint.sh
+++ init-containers/biogen-aws-to-gcp-agent/transporter-aws-to-gcp-agent/entrypoint.sh
@@ -5,8 +5,8 @@ declare -ra JAVA_OPTS_ARRAY=(
    -Dconfig.file=/etc/application.conf
    # Constrain memory.
    -server
-   -Xmx512m
-   -Xmx512m
+   -Xmx768m
+   -Xmx768m
    # GC settings (copied from Clio for now).
    -XX:+PrintFlagsFinal
    -XX:+PrintGCTimeStamps
