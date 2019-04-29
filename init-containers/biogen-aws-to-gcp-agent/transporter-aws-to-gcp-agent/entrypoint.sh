bc772ee62749437fdfbbbc08d64bae64245726ea
Dan Moran <danmoran@broadinstitute.org>
2019-04-29 13:10:03 -0400

    Reduce agent memory.

--- init-containers/biogen-aws-to-gcp-agent/transporter-aws-to-gcp-agent/entrypoint.sh
+++ init-containers/biogen-aws-to-gcp-agent/transporter-aws-to-gcp-agent/entrypoint.sh
@@ -5,8 +5,8 @@ declare -ra JAVA_OPTS_ARRAY=(
    -Dconfig.file=/etc/application.conf
    # Constrain memory.
    -server
-   -Xmx1g
-   -Xmx1g
+   -Xmx512m
+   -Xmx512m
    # GC settings (copied from Clio for now).
    -XX:+PrintFlagsFinal
    -XX:+PrintGCTimeStamps
